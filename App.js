910732152bd0b294c3862e0455ef6bc276389e77
Shivam Kakkar <shivamkakkar4@gmail.com>
2021-03-14 16:11:09 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {StatusBar, StyleSheet, Text} from 'react-native';
+
+// import {
+//   Colors,
+//   DebugInstructions,
+//   Header,
+//   LearnMoreLinks,
+//   ReloadInstructions,
+// } from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="#00aaff" />
+      <Text>Hello World</Text>
+    </>
+  );
+};
+
+export default App;
